8c5a807aba2e1cfdee3f134cab4643a94fd14717
Douglas Monteles <silvadouglas328@gmail.com>
2022-01-02 12:44:03 -0300

    Buscando Posts pelo título ignorando maiúsculas e minúsculas

--- workshopmongo/src/main/java/com/douglasmonteles/workshopmongo/resources/PostResource.java
+++ workshopmongo/src/main/java/com/douglasmonteles/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.douglasmonteles.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.douglasmonteles.workshopmongo.domain.Post;
+import com.douglasmonteles.workshopmongo.resources.utils.URL;
 import com.douglasmonteles.workshopmongo.services.PostService;
 
 @RestController
@@ -21,6 +25,16 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		var post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(
+				@RequestParam(value = "text", defaultValue = "") String text
+			) {
+		var textDecoded = URL.decodeParam(text); 
+		var posts = postService.findByTitle(textDecoded);
+		
+		return ResponseEntity.ok().body(posts);
 	}
 	
 }
